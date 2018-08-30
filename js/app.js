6386255dd03d6fc9350b02bc9d3e073fa39ab238
Bruno Torres <bruno.torres@gm3s.com.mx>
2018-08-30 07:39:52 -0500

    Primer Comit de mi pagina Web Personal

---
+++ js/app.js
@@ -0,0 +1,11 @@
+ $(".button-collapse").sideNav();
+ $(document).ready(function() {
+    window_size = $(window).height();
+    $('#izquierda').height(window_size);
+
+     
+});
+
+ $(document).ready(function(){
+      $('.slider').slider();
+    });
